9cb4f4c9bd32fab495f9eba39c03d03d1528f314
pipiyo <fe.cavieres@gmail.com>
2016-04-19 21:22:24 -0300

    cOMMIT PRUEBA CON EL YOLI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Grande Ogro' });
+  res.render('index', { title: 'CACA' });
 });
 
 module.exports = router;
